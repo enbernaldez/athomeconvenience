f109bc5903cb7784f9d81268c5562c7526337ffa
Edmarielle Bernaldez <edmariellenocedal.bernaldez@bicol-u.edu.ph>
2023-11-30 10:45:28 +0800

    removed unused impots

--- lib/navigation.dart
+++ lib/navigation.dart
@@ -7,7 +7,6 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:convex_bottom_bar/convex_bottom_bar.dart';
-import 'package:rxdart/rxdart.dart';
 
 class Navigation extends StatefulWidget {
   const Navigation({super.key});
